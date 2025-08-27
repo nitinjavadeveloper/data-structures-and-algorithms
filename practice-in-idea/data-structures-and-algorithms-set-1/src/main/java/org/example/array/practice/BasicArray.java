e2d41443502e16f37edc8fbc81625e982ffef319
NK119738 <nitinjavadeveloper@users.noreply.github.com>
2025-08-28 01:25:04 +0530

    Created the Structure

---
+++ practice-in-idea/data-structures-and-algorithms-set-1/src/main/java/org/example/array/practice/BasicArray.java
@@ -0,0 +1,17 @@
+package org.example.array.practice;
+
+/*
+ why array:
+ We used an array to represent multiple value as a single variable
+
+*/
+
+public class BasicArray {
+
+  public static void main(String[] args){
+    // structure
+    int[] array; // Initialization/ declaration of an array. Getting defin in the stack
+    array = new int[5]; // object creation and array created in the memory
+  }
+}
+
